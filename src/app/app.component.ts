0f8a4caa9964577626ab2f50bd851f3e5657e117
Matteo Nista <matteo.nista.99@gmail.com>
2022-06-13 11:00:58 +0200

    Improved Distinct Until Changed Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { distinctUntilChanged, from } from 'rxjs';
+import { Component } from '@angular/core';
+import { distinctUntilChanged, distinctUntilKeyChanged, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
+export class AppComponent {
+  private defaultComparison(): void {
     const obs$ = from(['angular', 'angular', 'tips', 'tips', '🎓']);
 
     obs$
       .pipe(distinctUntilChanged()) // Comparazione di default utilizzando '==='
       .subscribe(console.log); // angular - tips - 🎓
+  }
 
-    const newObs$ = from([
+  private objectComparison(): void {
+    const obs$ = from([
       { id: 123, name: 'John' },
       { id: 123, name: 'Greg' },
       { id: 456, name: 'Erik' },
     ]);
 
-    newObs$
-      .pipe(distinctUntilChanged((previous, current) => previous.id === current.id)) // Comparazione custom
+    obs$
+      .pipe(distinctUntilKeyChanged('id')) // Comparazione utilizzando la proprietà di un oggetto
       .subscribe(console.log); // { id: 123, name: 'John' } - { id: 456, name: 'Erik' }
+  }
+
+  private customComparison(): void {
+    const obs$ = from(['angular', 'Angular', 'tips', 'tips', '🎓']);
+
+    obs$
+      .pipe(distinctUntilChanged((previous, current) => previous.toLowerCase() === current.toLowerCase())) // Comparazione custom
+      .subscribe(console.log); // angular - tips - 🎓
   }
 }
